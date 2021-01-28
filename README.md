83889175e03b1bb5b54b22ca17c53e33d54f7e8f
Adrian Challinor <adrian.challinor@osiris.co.uk>
2021-01-28 08:57:59 +0000

    Add IP checking

---
+++ README.md
@@ -0,0 +1,198 @@
+# Monitor
+## A discreet remote data provider for CONKY
+
+Monitor is a small executable program that provides a number of endpoints that a [Conky server]
+ (https://en.wikipedia.org/wiki/Conky_(software)) can call to gather realtime monitoring data for display.
+
+ The enclosed screen shot shows 7 different boxes displayed on one background screen:
+ * On the RHS, BASTET is the name of the host machine
+ * In the centre, the four Odroid units are single board ARM computers or SBCs
+ * On the LHS, ISIS is a desktop PC.
+
+## Running monitor
+Monitor will runs as an executable program. It will run quite happily with no parameters at all. However, it can be customised by command line flags:
+`monitor 0.1.0
+AUTHOR: Adrian Challinor
+Remote monitor server, mainly for CONKY
+
+USAGE:
+    monitor [OPTIONS]
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -a, --address <address>    The IP address to bind to [default: 0.0.0.0]
+    -p, --port <port>          The IP port to bind to [default: 8000]
+    -w, --workers <workers>    The number of concurrent worker threads [default: 5]`
+
+To use a remote monitor, firstly, the monitor application must be installed and running on the remote computer. Sounds obvious, but the second part, it must be running, is a prequisite. It is left for the user to decide how to run it. You can start it on your desktop as you login; you can run it interactively (when you will see all the calls being reported); you can runit it with nohup; or you can build a startup service for systemd or init.d. *I may just provide some more support on this if people need*.
+
+## Interface to CONKY
+Once running, a Conky display can be created using **exec** and **execi** verbs. The *curl* command is used to connect to the remote monitor. The endpoint uri determines what dat awill be extracted and returned. For example:
+`SERVER: ${execi 3600 curl http://mybox:8000/name}`
+
+will connect to the monitor running on the standard port, 8000, on the box called **mybox**. You can use fully qualified names, short names from your /etc/hosts file, or even IP addresses. The *name* end point specifies that you want to see the computer name.
+
+For my Odroid SBCs, the full script for Conky is:
+`${color orange}SERVER:${color3}${exec curl http://o0:8000/name}
+${color orange}Version : ${color2}${exec curl http://o0:8000/os/versionname}
+${color orange}OS      : ${color2}${exec curl http://o0:8000/os/codename}
+${color orange}Kernel  : ${color2}${exec curl http://o0:8000/uname/r }
+${color orange}Uptime  : ${color2}${execi 20 curl http://o0:8000/uptime }
+${color orange}CPU Load Av.: ${color2}${execi 60 curl http://o0:8000/loadavg}
+${color orange}CPU load: ${color2}${execi 60 curl http://o0:8000/cpuload}
+${color orange}Core 0: ${color2}${execi 60 curl http://o0:8000/temp/0}°C ${color orange}Core 1: ${color2}${execi 60 curl http://o0:8000/temp/1}°C ${color orange}Core 2: ${color2}${execi 60 curl http://o0:8000/temp/2}°C ${color orange}Core 3: ${color2}${execi 60 curl http://o0:8000/temp/3}°C ${color orange}GPU   : ${color2}${execi 60 curl http://o0:8000/temp/4}°C
+${color orange}/     ${color2}${execi 60 curl -X GET http://o0:8000/disk/root/usedp} of ${execi 60 curl -X GET http://o0:8000/disk/root/total}
+${color orange}/data ${color2}${execi 60 curl -X GET http://o0:8000/disk/data/usedp} of ${execi 60 curl -X GET http://o0:8000/disk/data/total}
+${hr 1}`
+
+I know, you are going to get long lines with Conky. Sorry about that, but frankly, tough.
+
+## What can be monitored
+Monitor is a work in progress. I created it to give me the information I wanted to see, but it is being upgraded and enhanced all the time. All returns are formatted as sent as character strings. The curent list of end points are:
+
+'
+ENDPOINT                Description
+==============          =====================================
+/status                 Returns the fact Monitor is running and its version
+
+/boot                   Boot time
+
+/os/name                The name of the current os, eg Linux
+
+/os/version             The version as numeric values, eg 18.10
+
+/os/versionname         The version as a name, eg "18.10 Cosmic Cuttlefish"
+
+/os/codename            The code name, eg Cosmic
+
+/temp/<ID>              Applies to ARM computers only.
+                        See Sensors below
+
+/sensors/<chip>/<param> Applies AMD motherboards confirmed.
+                        See sensors section below
+
+/uptime                 Formatted string of the uptime of the box
+
+/aptcheck               Applies to Ubuntu boxes only
+                        Reports if any updates are pending
+
+/aptcheckbrief          Like aptcheck, it reports the changes pending, but as a 
+                        single line. It does split the changes to show how many are 
+                        security release.
+
+/reboot                 This does NOT reboot the remote server! 
+                        It reports if the automatic installation of required updates 
+                        means that the server has to be rebooted. 
+
+/disk/<disk>/<param>    The <disk> is the name of the disk you want info about
+                        but omit the leading "/". For the system disk, use the special name "root".
+
+                        <param> is parameter you want info about:
+                            avail       Available disk space
+                            total       Total disk space
+                            free        Amount that is free
+                            freep       The percentage that is free
+                            usedp       The percentage that is used
+                            files       The number of files (inodes)
+
+/cpu                    Total CPU usage as a percentage. Note that getting
+                        remote CPU usage by individual core is not (yet)
+                        supported.
+
+/cpuload                The load load as percentages of:
+                        user, nice, system, interupt and idle process time
+
+/loadavg                The average load over the last 1, 5 and 15 minutes
+
+/memory                 The memory usage, as absolute numbers, of how much is 
+                        used, and how much is free. 
+            
+/ip                     The main IP address of the server. For servers with 
+                        multiple IP's, this will normally be the one which is 
+                        used to connect to the internet. 
+
+/port/<number>          Checks if there is a service on this port that is 
+                        accepting connection. For example, 6379 will check for 
+                        a Redis server, whereas 80 will check for a web server. 
+
+/uname/<param>          Interrogate the uname command. The params relate to the 
+                        infomation to return: 
+                            n   - Node name 
+                            s   - System name - normally Linux
+                            r   - OS Version
+                            v   - The full OS Version Name 
+                            m   - Machine architecture
+
+
+## Hardware Sensors
+Not all PC's are created equally. Indeed, not all Linux based PC's are the same. Leastways, not when it comes to handling the hardware sensor values. For example, all though some of my SBC run Ubuntu, they do not have access to /usr/bin/sensors, nor to the sensorsd linkable library. Furthermore, diffrent motherboards and chipsets use different methods to report fan speed and cpu temperatures. Its pretty much a free for all out there.
+
+Monitor approaches this in a pragmatic way. It provides two different interfaces:
+    * /sensors is used for AMD motherboards. Intel - watch this space
+    * /cpu  is used for ARM processors
+
+###/sensors
+The motherboard and different PCI boards can all provide different information and all have different ways of accessing the various parameters. They are accessed via an endpoint for each parameter. To find out what endpoints are available, we have provided a small executable that will list the availble
+endpoints on your motherboard. **Don't copy mine, or anyone elses**. Use the
+**sensors** program to see what is available. The output gives you the URI you need to call.
+
+On my system, **sensors** gives:
+
+`bastet : Listing all sensors in conky monitor format
+
+http://bastet:8000/nouveau-pci-0100/GPU_core = +0.91
+http://bastet:8000/nouveau-pci-0100/temp1 = +47.0°C
+http://bastet:8000/k10temp-pci-00c3/CPU_Temp = +29.8°C
+http://bastet:8000/it8721-isa-0290/12V = +11.65
+http://bastet:8000/it8721-isa-0290/5V = +4.78
+http://bastet:8000/it8721-isa-0290/Vcore = +1.36
+http://bastet:8000/it8721-isa-0290/3_3V = +3.29
+http://bastet:8000/it8721-isa-0290/VDDA = +2.52
+http://bastet:8000/it8721-isa-0290/Vbat = +3.36
+http://bastet:8000/it8721-isa-0290/CPU_Fan1 = 1163
+http://bastet:8000/it8721-isa-0290/CPU_Fan2 = 890
+http://bastet:8000/it8721-isa-0290/Chassis_Fan = 998
+http://bastet:8000/it8721-isa-0290/CPU_Temp = +34.0°C
+http://bastet:8000/it8721-isa-0290/M_B_Temp = +32.0°C
+http://bastet:8000/nouveau-pci-0600/GPU_core = +0.90
+http://bastet:8000/nouveau-pci-0600/temp1 = +50.0°C
+http://bastet:8000/asus-isa-0000/cpu_fan = 0
+http://bastet:8000/fam15h_power-pci-00c4/power1 = 86.88
+`
+###ARM processors
+ARM systems, in fact all SBCs I have come across, do not support the sensors interface. Instead, the board data is acessed via the system files. Depending on your board and the operating system you are running, different configurations may be necessary.
+
+For ODROID XU4, running Ubuntu 18.4 LTS the endpoints are:
+`/temp/<ID>`
+
+where <ID> is
+    1.  Core 1
+    2.  Core 2
+    3.  Core 3
+    4.  Core 4
+    5.  GPU
+Note that it has 8 cores, but they are split major and minor cores. The CPU die packages a majore and a minor core together as one, and the temperature of the two together is reported as a major core.
+
+For Raspberry PI installation - *watch this space*
+
+ ## Installation
+ The **monitor** and **sensors** applications are RUST programs. You can download the compiled versions from **WEBSITE TO BE SUPPLIED**. These will work if and only if the compiled version matches your running system. Otherwise, you will get a rude link/loader error message that the GLIBC version does not match. If that is the case, you will have to do the following:
+
+ 1. [Install RUST](https://www.rust-lang.org/tools/install). Don't panic. It is ridiculously simple to do.
+
+ 2. Download and expand Monitor (and Sensors) from  GIT
+
+ 3. Change direcory to where the Monitor code is installed, you should see a src directory and some Cargo.toml files.
+
+ 4. Execute the command **cargo build --release**
+
+ 5. If you get problems, contact me for assistance
+
+ 6. Otherwise, copy the file **./target/release/monitor** to wherever you want to run it from. You're now good to go.
+
+To build a system that does not depend on GLIB
+rustup target add x86_64-unknown-linux-musl --toolchain=nightly
+cargo build --target x86_64-unknown-linux-musl --release
---
+++ src/main.rs
@@ -0,0 +1,489 @@
+/*
+ * Created on Tue Dec 11 2018
+ * Author Adrian Challinor FRAS
+ *
+ * Copyright (c) 2018 Osiris Consultants Ltd ALL RIGHTS RESERVED
+ */
+
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate clap;
+
+extern crate chrono;
+extern crate hostname;
+extern crate libc;
+extern crate uname;
+extern crate uptime_lib;
+
+use clap::{App, Arg};
+use hostname::get_hostname;
+use libc::c_char;
+use libc::statvfs;
+use rocket::config::{Config, Environment, Limits, LoggingLevel};
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::net::{TcpStream, UdpSocket};
+
+use std::mem;
+use std::path::Path;
+use std::process;
+use std::process::Command;
+use std::thread;
+use std::time::Duration;
+use systemstat::{Platform, System};
+use uname::uname;
+
+fn main() {
+    let cmd_arguments = App::new("monitor")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Remote monitor server, mainly for CONKY")
+        .arg(
+            Arg::with_name("address")
+                .short("a")
+                .long("address")
+                .help("The IP address to bind to")
+                .default_value("0.0.0.0"),
+        )
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .long("port")
+                .help("The IP port to bind to")
+                .default_value("9000"),
+        )
+        .arg(
+            Arg::with_name("sensors")
+                .short("s")
+                .long("sensors")
+                .help("List available sensors and exit"),
+        )
+        .arg(
+            Arg::with_name("workers")
+                .short("w")
+                .long("workers")
+                .help("The number of concurrent worker threads")
+                .default_value("5"),
+        )
+        .get_matches();
+
+    let address = cmd_arguments.value_of("address").unwrap();
+    let port: u16 = cmd_arguments
+        .value_of("port")
+        .unwrap()
+        .parse::<u16>()
+        .unwrap();
+    let workers: u16 = cmd_arguments
+        .value_of("workers")
+        .unwrap()
+        .parse::<u16>()
+        .unwrap();
+
+    if cmd_arguments.is_present("sensors") {
+        show_sensors(port);
+        process::exit(1);
+    }
+
+    let config = Config::build(Environment::Production)
+        .log_level(LoggingLevel::Critical)
+        .address(address)
+        .port(port)
+        .workers(workers)
+        .limits(Limits::new().limit("forms", 8))
+        .unwrap();
+
+    rocket::custom(config)
+        .mount("/status", routes![status])
+        .mount("/name", routes![name])
+        .mount("/os/name", routes![os_name])
+        .mount("/os/version", routes![os_version])
+        .mount("/os/versionname", routes![os_vernamename])
+        .mount("/os/codename", routes![os_codename])
+        .mount("/temp", routes![temp])
+        .mount("/sensors", routes![sensors])
+        .mount("/uptime", routes![uptime])
+        .mount("/aptcheck", routes![aptcheck])
+        .mount("/aptcheckbrief", routes![aptcheckbrief])
+        .mount("/reboot", routes![reboot])
+        .mount("/disk", routes![disk])
+        .mount("/loadavg", routes![load])
+        .mount("/cpuload", routes![cpuload])
+        .mount("/cpu", routes![cpu])
+        .mount("/boot", routes![boot])
+        .mount("/uname", routes![unameinfo])
+        .mount("/memory", routes![memory])
+        .mount("/ip", routes![local_ip])
+        .mount("/port", routes![port])
+        .register(catchers![not_found])
+        .launch();
+}
+
+#[get("/")]
+fn local_ip() -> String {
+    let socket = match UdpSocket::bind("0.0.0.0:0") {
+        Ok(s) => s,
+        Err(_) => return "None".to_string(),
+    };
+
+    match socket.connect("8.8.8.8:80") {
+        Ok(()) => (),
+        Err(_) => return "None".to_string(),
+    };
+
+    match socket.local_addr() {
+        Ok(addr) => return addr.ip().to_string(),
+        Err(_) => return "None".to_string(),
+    };
+}
+
+fn check_port_by_ip(ipaddr: &str, port: u16) -> bool {
+    match TcpStream::connect((ipaddr, port)) {
+        Ok(_) => true,
+        Err(_) => false
+    }
+}
+
+#[get("/<port>")]
+fn port(port: u16) -> String  {
+    let mut found = check_port_by_ip("0.0.0.0", port);
+    if !found {found = check_port_by_ip("localhost", port) };
+    if !found {found = check_port_by_ip(&local_ip(), port) };
+
+    match found {
+        true => format!("Up"),
+        false => format!("Down")
+    }
+}
+
+#[get("/<param>")]
+fn unameinfo(param: String) -> String {
+    let info = uname().unwrap();
+    format!(
+        "{}",
+        match param.as_str() {
+            "n" => info.nodename,
+            "s" => info.sysname,
+            "r" => info.release,
+            "v" => info.version,
+            "m" => info.machine,
+            _ => "Param?".to_string(),
+        }
+    )
+}
+
+#[get("/")]
+fn memory() -> String {
+    let sys = System::new();
+    match sys.memory() {
+        Ok(mem) => format!("\nMemory total: {} free: {} )", mem.total, mem.free),
+
+        Err(x) => format!("\nMemory error: {}", x),
+    }
+}
+
+#[get("/")]
+fn boot() -> String {
+    let sys = System::new();
+    match sys.boot_time() {
+        Ok(boot_time) => format!("\nBoot time: {}", boot_time),
+        Err(x) => format!("\nBoot time: error: {}", x),
+    }
+}
+
+#[get("/")]
+fn load() -> String {
+    let sys = System::new();
+    match sys.load_average() {
+        Ok(loadavg) => format!(
+            "\nLoad average: {} {} {}",
+            loadavg.one, loadavg.five, loadavg.fifteen
+        ),
+        Err(x) => format!("\nLoad average: error: {}", x),
+    }
+}
+
+#[get("/")]
+fn cpuload() -> String {
+    let sys = System::new();
+    match sys.cpu_load_aggregate() {
+        Ok(cpu) => {
+            thread::sleep(Duration::from_secs(1));
+            let cpu = cpu.done().unwrap();
+            format!(
+                "CPU load: {}% user, {}% nice, {}% system, {}% intr, {}% idle ",
+                (cpu.user * 100.0) as i64,
+                (cpu.nice * 100.0) as i64,
+                (cpu.system * 100.0) as i64,
+                (cpu.interrupt * 100.0) as i64,
+                (cpu.idle * 100.0) as i64
+            )
+        }
+        Err(x) => format!("\nCPU load: error: {}", x),
+    }
+}
+
+#[get("/")]
+fn cpu() -> String {
+    let sys = System::new();
+    match sys.cpu_load_aggregate() {
+        Ok(cpu) => {
+            thread::sleep(Duration::from_secs(1));
+            let cpu = cpu.done().unwrap();
+            format!("{}", 100 - (cpu.idle * 100.0) as i64)
+        }
+        Err(x) => format!("\nCPU error: {}", x),
+    }
+}
+
+#[get("/<mountpoint>/<param>")]
+fn disk(mountpoint: String, param: String) -> String {
+    let mut info: statvfs = unsafe { mem::zeroed() };
+    let target = if mountpoint == "root" {
+        format!("/\0")
+    } else {
+        format!("/{}\0", mountpoint)
+    };
+    let result = unsafe { statvfs(target.as_ptr() as *const c_char, &mut info) };
+
+    match result {
+        0 => match param.as_str() {
+            "avail" => format!("{}", byte_size(info.f_bavail as u64 * info.f_bsize as u64)),
+            "total" => format!("{}", byte_size(info.f_blocks as u64 * info.f_bsize as u64)),
+            "free" => format!("{}", byte_size(info.f_bfree as u64 * info.f_bsize as u64)),
+            "freep" => format!("{}%", info.f_bfree as u64 * 100 / info.f_blocks as u64),
+            "usedp" => format!(
+                "{}%",
+                100 - (info.f_bfree as u64 * 100 / info.f_blocks as u64)
+            ),
+            "files" => format!("{}", info.f_files as u64),
+            _ => format!("unknown param"),
+        },
+        _ => format!("io error on disk: {}", target),
+    }
+}
+
+fn byte_size(ival: u64) -> String {
+    let mut val = ival as f64;
+    let units = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    let mut p = 0;
+    while val > 1024.0 {
+        p = p + 1;
+        val = val / 1024.0;
+    }
+
+    format!("{} {}", val.round() as u64, units[p])
+}
+
+#[get("/")]
+fn aptcheck() -> String {
+    let output = {
+        Command::new("/usr/lib/update-notifier/apt-check")
+            .arg("--human-readable")
+            .output()
+            .expect("Failed")
+    };
+    let s = String::from_utf8_lossy(&output.stdout);
+
+    s.to_string()
+}
+
+#[get("/")]
+fn aptcheckbrief() -> String {
+    let output = {
+        Command::new("/usr/lib/update-notifier/apt-check")
+            .output()
+            .expect("Failed")
+    };
+    let apt = String::from_utf8_lossy(&output.stderr);
+    let mut split = apt.split(";");
+    format!("Installable: {} - Security: {}", split.next().unwrap(), split.next().unwrap())
+}
+
+#[get("/")]
+fn reboot() -> String {
+    let mut s = "";
+    if Path::new("/var/run/reboot-required").exists() {
+        s = "REBOOT REQUIRED";
+    }
+    s.to_string()
+}
+
+#[get("/")]
+fn uptime() -> String {
+    match uptime_lib::get() {
+        Ok(uptime) => {
+            let s: u64 = (uptime.num_milliseconds() as f64 / 1000.0) as u64;
+            let m: u64 = s / 60;
+            let sd = s - (m * 60);
+            let h: u64 = m / 60;
+            let md = m - (h * 60);
+            let d = h / 24;
+            let hd = h - (d * 24);
+
+            format!("{}d {}h {}m {}s", d, hd, md, sd)
+        }
+        Err(err) => format!("uptime: {}", err),
+    }
+}
+
+#[get("/<chipname>/<param>")]
+fn sensors(chipname: String, param: String) -> String {
+    let mut rc = "404-Feature not found".to_string();
+    if Path::new("/usr/bin/sensors").exists() {
+        let output = {
+            Command::new("/usr/bin/sensors")
+                .arg("-A")
+                .output()
+                .expect("Failed")
+        };
+        let mut device: &str = "";
+        String::from_utf8(output.stdout)
+            .unwrap()
+            .lines()
+            .for_each(|x| {
+                if !x.is_empty() && !x.starts_with(' ') && !x.starts_with('(') {
+                    if x.contains(":") {
+                        let v: Vec<&str> = x.split(':').collect();
+                        let pname = v[0]
+                            .replace("/", "_")
+                            .replace(" ", "_")
+                            .replace("+", "")
+                            .replace(".", "_");
+
+                        if chipname.eq_ignore_ascii_case(device)
+                            && param.eq_ignore_ascii_case(pname.as_str())
+                        {
+                            let vals: Vec<&str> = v[1].trim().split(' ').collect();
+                            rc = format!("{}", vals[0]);
+                        }
+                    } else {
+                        device = x;
+                    }
+                }
+            });
+    }
+    rc
+}
+
+#[get("/<id>")]
+fn temp(id: String) -> String {
+    let sensor = format!("/sys/devices/virtual/thermal/thermal_zone{}/temp", id);
+    let f = match File::open(sensor) {
+        Ok(file) => file,
+        Err(_) => return format!("404 - Not odroid XU4 "),
+    };
+    let mut file = BufReader::new(&f);
+    let mut input = String::new();
+    file.read_line(&mut input).expect("Sensor not accessible");
+    let line = input.trim();
+    println!("Sensor {} = <{}>", id, line);
+
+    let temp = line.parse::<i32>().unwrap() / 1000;
+    format!("{}", temp)
+}
+
+#[get("/")]
+fn os_name() -> String {
+    get_param("NAME")
+}
+
+#[get("/")]
+fn os_version() -> String {
+    get_param("VERSION_ID")
+}
+
+#[get("/")]
+fn os_codename() -> String {
+    get_param("UBUNTU_CODENAME")
+}
+
+#[get("/")]
+fn os_vernamename() -> String {
+    get_param("VERSION")
+}
+
+fn get_param(key: &str) -> String {
+    let f = match File::open("/etc/os-release") {
+        Ok(file) => file,
+        Err(e) => return format!("404 - os-release {} ", e),
+    };
+
+    let file = BufReader::new(&f);
+    for line in file.lines() {
+        let l = line.unwrap();
+        let part: Vec<&str> = l.split('=').collect();
+        if part[0] == key {
+            return part[1].to_string().replace(r#"""#, "");
+        }
+    }
+    format!("404 - key {} not found", key)
+}
+
+/// Basic status
+#[get("/")]
+fn status() -> String {
+    format!("Status - Running - {}", env!("CARGO_PKG_VERSION"))
+}
+
+/// Get the nost name
+#[get("/")]
+fn name() -> String {
+    match get_hostname() {
+        Some(name) => name,
+        None => panic!("No host name"),
+    }
+}
+
+/// Catch 404 errors
+#[catch(404)]
+fn not_found() -> String {
+    format!("404 - Sorry, I can't do that Dave.")
+}
+
+fn show_sensors(port: u16) {
+    let mut found = false;
+    let hostname = get_hostname().unwrap();
+    println!("{} : Listing all sensors in conky monitor format", hostname);
+    println!("");
+
+    if Path::new("/usr/bin/sensors").exists() {
+        let output = {
+            Command::new("/usr/bin/sensors")
+                .arg("-A")
+                .output()
+                .expect("Failed")
+        };
+        let mut device: &str = "";
+        String::from_utf8(output.stdout)
+            .unwrap()
+            .lines()
+            .for_each(|x| {
+                if !x.is_empty() && !x.starts_with(' ') && !x.starts_with('(') {
+                    if x.contains(":") {
+                        found = true;
+                        let v: Vec<&str> = x.split(':').collect();
+                        let param = v[0]
+                            .replace("/", "_")
+                            .replace(" ", "_")
+                            .replace("+", "")
+                            .replace(".", "_");
+                        let vals: Vec<&str> = v[1].trim().split(' ').collect();
+                        let val = vals[0];
+                        println!(
+                            "http://{}:{}/{}/{} = {}",
+                            hostname, port, device, param, val
+                        );
+                    } else {
+                        device = x;
+                    }
+                }
+            });
+    }
+    if !found {
+        println!("No sensors found");
+    }
+}
